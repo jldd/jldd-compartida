3b4fd4ec68c6bc4a0750a84b812900f90a5d0b80
jose <pccomputer100@gmail.com>
2017-03-13 07:44:35 +0100

    añadido lineas 3

--- src/claseende1/ClaseEnde1.java
+++ src/claseende1/ClaseEnde1.java
@@ -8,6 +8,7 @@ public class ClaseEnde1 {
     public static void main(String[] args) {
         System.out.println("inicio de un proyecto");
         System.out.println("inicio de un proyecto 2");
+        System.out.println("inicio de un proyecto 3");
     }
     
 }
